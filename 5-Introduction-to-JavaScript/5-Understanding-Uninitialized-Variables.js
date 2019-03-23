5fb7eb2fc0c1f50d2068ca0af6c39d202a482db6
Kat Grennan <turret24@gmail.com>
2019-03-23 13:33:04 -0600

    Initilaize variables a, b, c

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// Already known
+
+// Initialize these three variables
+var a = 5,
+    b = 10,
+    c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
