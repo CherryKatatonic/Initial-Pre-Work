b9bed275e4f63b1d91b3a7483eaa3b6806d156c5
Kat Grennan <turret24@gmail.com>
2019-03-23 15:49:52 -0600

    Build a dog

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Already known
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  'name': 'Doggo',
+  'legs': 4,
+  'tails': 1,
+  'friends': ['Mailman', 'Squirrel', 'Cat']
+};
