8fe87aad8e1e6415851f1a14a8736bc4558a5b3a
Kat Grennan <turret24@gmail.com>
2019-03-23 15:01:40 -0600

    Combine if statements

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,15 @@
+// Already known
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
