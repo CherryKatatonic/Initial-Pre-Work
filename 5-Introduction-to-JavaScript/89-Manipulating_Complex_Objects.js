49e94f29cc8c6bcda1521253b876e7dd0dc16950
Kat Grennan <turret24@gmail.com>
2019-03-23 16:11:02 -0600

    Create new album in myMusic

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+// Already known
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    'artist': 'Bring Me The Horizon',
+    'title': 'Sempiternal',
+    'release_year': 2013,
+    'formats': [
+      'CD',
+      'MP3'
+    ]
+  }
+];
