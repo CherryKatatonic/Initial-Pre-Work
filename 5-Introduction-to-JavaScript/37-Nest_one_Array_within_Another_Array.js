91dec107d0bd600af78a2ef035fd564b90683f10
Kat Grennan <turret24@gmail.com>
2019-03-23 14:27:04 -0600

    Create nested arrray

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// Already known
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [['pilots', 21], ['maroon', 5]];
