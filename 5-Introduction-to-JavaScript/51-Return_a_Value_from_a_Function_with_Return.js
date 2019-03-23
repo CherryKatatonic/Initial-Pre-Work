63b7eed9730cb3486ab9db798e902c2b6ea9e99f
Kat Grennan <turret24@gmail.com>
2019-03-23 14:45:30 -0600

    Return value from function

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+// Already known
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
