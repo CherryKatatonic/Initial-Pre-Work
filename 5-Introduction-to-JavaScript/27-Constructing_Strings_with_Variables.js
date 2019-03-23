e82b86616aec8925c42862fba04997c39fbd9e25
Kat Grennan <turret24@gmail.com>
2019-03-23 14:01:32 -0600

    Construct String with variable

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// Already known
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = 'Slim Shady';
+var myStr = 'My name is ' + myName + ' and I am well!';
