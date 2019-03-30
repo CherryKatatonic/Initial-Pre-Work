fe086700e8b565f387a2e219420f4df4f187fc48
Kat Grennan <turret24@gmail.com>
2019-03-30 13:26:27 -0600

    Use simple fields to declare object literal

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,9 @@
+// Learned how to use simple fields with object literals
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {name, age, gender};
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
